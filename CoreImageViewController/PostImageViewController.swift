0090ba390242a35c1937ada04c005212ddf901d6
Ezra Black <dev.ezrablack@gmail.com>
2020-06-01 16:50:19 -0500

    Collection View Implementation
    Added A Collection view to the view controller - 
    Implemented Delegates, DataSources, &  Temporary Constraints -

--- CoreImageViewController/PostImageViewController.swift
+++ CoreImageViewController/PostImageViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Ezra Black on 6/1/20.
 //  Copyright © 2020 Casanova Studios. All rights reserved.
 //
-
+import Foundation
 import UIKit
 import CoreImage
 import CoreImage.CIFilterBuiltins
@@ -13,16 +13,44 @@ import Photos
 
 class PostImageViewController: UIViewController {
     
+    fileprivate let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Filtercell")
+        return cv
+    }()
+    
     private let context = CIContext(options: nil)
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .white
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
     }
-    
+}
 
+extension PostImageViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/2.5, height: collectionView.frame.width/2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Filtercell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
 }
