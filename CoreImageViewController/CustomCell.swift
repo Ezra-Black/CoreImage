c6fd92dfc2dbd0e7e495c7dee9c906f2dd073646
Ezra Black <dev.ezrablack@gmail.com>
2020-06-01 17:01:00 -0500

    #bug
    Custom Cell with a failing custom button being implemented.

---
+++ CoreImageViewController/CustomCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomCell.swift
+//  CoreImageViewController
+//
+//  Created by Ezra Black on 6/1/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    
+    fileprivate let button: UIButton = {
+        let buttonToUse = UIButton()
+        buttonToUse.translatesAutoresizingMaskIntoConstraints = false
+        buttonToUse.clipsToBounds = true
+        return buttonToUse
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init decoder:) has not been implemented")
+    }
+}
