34d029e6ce9badbeeb34e4d3a6f009cc1a316314
Ezra Black <dev.ezrablack@gmail.com>
2020-06-01 20:13:06 -0500

    Added Protocol for the custom cell to be able to send information when a button is tapped -

---
+++ CoreImageViewController/Helper Filers/Protocols.swift
@@ -0,0 +1,14 @@
+//
+//  Protocols.swift
+//  CoreImageViewController
+//
+//  Created by Ezra Black on 6/1/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import Foundation
+
+protocol ImageCustomCollectionCellDelegate {
+    func didTapButtonInside(cell: CustomCell)
+}
+
