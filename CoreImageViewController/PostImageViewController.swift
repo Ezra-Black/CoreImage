ab02aaaef369fa4fdc9f03b5f2dca872a4aa39f8
Ezra Black <dev.ezrablack@gmail.com>
2020-06-02 18:55:27 -0500

    Updates

--- CoreImageViewController/PostImageViewController.swift
+++ CoreImageViewController/PostImageViewController.swift
@@ -15,15 +15,27 @@ class PostImageViewController: UIViewController {
     
     //MARK: Properties -
     
+    private var originalImage : UIImage? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     private let context = CIContext(options: nil)
     
     //MARK: Outlets -
     
     @IBOutlet weak var slider1Label: UILabel!
     @IBOutlet weak var slider2Label: UILabel!
+    @IBOutlet weak var slider1: UISlider!
+    @IBOutlet weak var slider2: UISlider!
+    @IBOutlet weak var slider3: UISlider!
+    @IBOutlet weak var slider4: UISlider!
+    @IBOutlet weak var slider5: UISlider!
     @IBOutlet weak var photoImageView: UIImageView!
     //MARK: IBActions -
-    @IBAction func slider1(_ sender: Any) {
+    @IBAction func slider1(_ sender: UISlider) {
+        updateViews()
     }
     @IBAction func slider2(_ sender: Any) {
     }
@@ -37,19 +49,43 @@ class PostImageViewController: UIViewController {
     }
     @IBAction func savePhotoButtonPressed(_ sender: Any) {
     }
+    
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         let filter1 = CIFilter.colorMonochrome()
         filter1.intensity = 1
         print(filter1.attributes)
-        
+        originalImage = photoImageView.image
+    }
+    
+    func updateViews() {
+        if let originalImage = originalImage {
+            photoImageView.image = filterImage(originalImage)
+        } else {
+            photoImageView.image = UIImage(named: "selfie")
+        }
     }
     
     //MARK: Methods -
     
+    
+    
     func filterImage(_ image: UIImage) -> UIImage? {
-        return nil
+        
+        guard let cgImage = image.cgImage else { return nil }
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        let filter = CIFilter.colorMonochrome()
+        filter.inputImage = ciImage
+        filter.intensity = slider1.value
+        
+        guard let outputCIImage = filter.outputImage else { return nil }
+        guard let outputCGIImage = context.createCGImage(outputCIImage,
+                                                         from: CGRect(origin: .zero,
+                                                                      size: originalImage!.size)) else {
+                                                                        return nil }
+        return UIImage(cgImage: outputCGIImage)
     }
-
 }
 
