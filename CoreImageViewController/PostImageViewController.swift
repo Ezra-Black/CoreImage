53e7027d0e4c0340f9b239bfe162330012d13c3d
Ezra Black <dev.ezrablack@gmail.com>
2020-06-02 15:10:33 -0500

    Added a SB With a collection view - added image view - added sliders and labels - added outlets and actions respectively -

--- CoreImageViewController/PostImageViewController.swift
+++ CoreImageViewController/PostImageViewController.swift
@@ -13,6 +13,23 @@ import Photos
 
 class PostImageViewController: UIViewController {
     
+    //MARK: Outlets -
+    
+    @IBOutlet weak var slider1Label: UILabel!
+    @IBOutlet weak var slider2Label: UILabel!
+    @IBOutlet weak var slider3Label: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
+    
+    //MARK: Actions -
+    @IBAction func slider1(_ sender: Any) {
+    }
+    @IBAction func slider2(_ sender: Any) {
+    }
+    @IBAction func slider3(_ sender: Any) {
+    }
+    
     private let context = CIContext(options: nil)
     
     
